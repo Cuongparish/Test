d18acba0ab248c28fecd237321665c589fa143dc
Trịnh Quốc Cường <cuongparish@gmail.com>
2024-09-07 11:00:36 +0700

    done question 4

---
+++ question4/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("ab");
+                }
+                Console.WriteLine('a');
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine('b');
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
